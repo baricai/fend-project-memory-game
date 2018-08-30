f0ce5d02c97b31c1a403f7cc5cf744b7dd1ecb67
baricai <40843695+baricai@users.noreply.github.com>
2018-08-29 21:52:12 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@
  * Create a list that holds all of your cards
  */
 
-let card = document.getElementsByClassName("card");
 
 let cards = ['fa fa-star', 'fa fa-star', 'fa fa-diamond', 'fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-bolt', 'fa fa-cube', 'fa fa-cube', 'fa fa-leaf', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bicycle', 'fa fa-bomb', 'fa fa-bomb'];
 
