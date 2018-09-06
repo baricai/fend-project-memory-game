ed3d14c566bf3a9fc36cbb044191ac5126924a62
baricai <40843695+baricai@users.noreply.github.com>
2018-09-05 22:13:42 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
   // Define the symbols and create a deck of cards holding two of each symbol
 var cards = ["fa fa-diamond", "fa fa-paper-plane-o","fa fa-anchor", "fa fa-bolt","fa fa-cube","fa fa-anchor", "fa fa-leaf", "fa fa-bicycle", "fa fa-diamond", "fa fa-bomb", "fa fa-leaf", "fa fa-bomb", "fa fa-bolt", "fa fa-bicycle", "fa fa-paper-plane-o", "fa fa-cube"];
-var grid = [];  
+var grid = [...cards,...cards];  
 
     var openedCards = [];
     var matchCounter = 0;
