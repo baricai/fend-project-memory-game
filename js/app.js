75cc90f3c335e34d8dde97bcd8c61b340ab18c7a
baricai <40843695+baricai@users.noreply.github.com>
2018-09-09 21:50:26 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,5 @@
- /*
- * Create a list that holds all of your cards
- */
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+ // Define the symbols and create a deck of cards holding two of each symbol
+
 var cards = ["fa fa-diamond", "fa fa-paper-plane-o","fa fa-anchor", "fa fa-bolt","fa fa-cube","fa fa-anchor", "fa fa-leaf", "fa fa-bicycle", "fa fa-diamond", "fa fa-bomb", "fa fa-leaf", "fa fa-bomb", "fa fa-bolt", "fa fa-bicycle", "fa fa-paper-plane-o", "fa fa-cube"];
 var grid = [];  
 
@@ -17,6 +10,10 @@ var star = 3;
 var tryCounter = 0;
 var timeInt = 0;
 
+// 1. Resets the game to default values
+// 2. Remove the old deck of cards
+// 3. Create a new shuffled deck of cards
+// 4. Hide the Congrats popup
 function reset() {
     openedCards = [];
     matchCounter = 0;
@@ -30,33 +27,7 @@ function reset() {
     hideCongrats();
 }
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
-
+// Grab the score-panel, add a timer with default value of 00:00, and initialize the total seconds to 0
 const timer = document.createElement("div");
 timer.className = "timer";
 timer.innerHTML = "00:00";
@@ -83,7 +54,19 @@ var restart = document.getElementsByClassName("fa-repeat");
  */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
 
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
 
 // Call the buildCongrats function once
 buildCongrats();
@@ -312,4 +295,3 @@ function resetStars() {
         stars[i].className = "fa fa-star";
     }
 }
-
