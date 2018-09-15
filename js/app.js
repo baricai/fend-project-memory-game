54c7a1c3fa4344eb0764de278517b30a57a95225
baricai <40843695+baricai@users.noreply.github.com>
2018-09-15 08:03:21 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 /*
+ *Create a list that holds the cards
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
@@ -61,7 +62,7 @@ const panel = document.getElementsByClassName("score-panel");
 panel[0].appendChild(timer);
 let totalSeconds = 0;
 
-// Grab the deck div element from the HTML
+
 var deck = document.getElementsByClassName("deck");
 
 // Grab the 'moves' from the HTML and change the text to 0
@@ -72,19 +73,7 @@ moves[0].innerHTML = 0;
 var restart = document.getElementsByClassName("fa-repeat");
 
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
-
-
-// Call the buildCongrats function once
-
-
-// Add eventlistener to listen for click on reset button
 restart[0].addEventListener("click", reset);
 
 // Call the reset function when page first loads
@@ -92,14 +81,12 @@ reset();
 
 
 
-// Clear the old deck of cards by removing the HTML elements passed into it
+
 function clearDeck(deck) {
     deck[0].remove();
 }
 
-// Create a HTML list elements based on the deck that's passed into it.
-// It adds the proper classname to each card, add an eventlistener to each card
-// It then appends the list to the webpage
+
 function createDeckHTML(deck) {
     var ul = document.createElement("ul");
     ul.className = `deck`;
@@ -117,55 +104,46 @@ function createDeckHTML(deck) {
 }
 
 
-// When a user clicks on a card for the first time, it starts the game timer
-// - it displays the card if it has not been opened or matched already
-// - it allow only two cards to be displayed at a time
-// - if two cards are clicked, it checks if it is a match
-// - if all cards are matched, the user won the game and a congrats message popup
-// - if the two cards are no match, it hides the cards
+
 function processClick() {
-    // Test 1: User can only open two cards at a time
-    // Test 2: User can not click the same card
-    // Test 3: User can not click already matched cards
+    
     if ((openedCards.length < 2) && (!isSameCard(this)) && (!isAlreadyMatched(this)) ) {
-        // Count the number of clicks that do not result a match
+       
         tryCounter++;
 
         displayCard(this);
         addOpenedList(this);
         incrementCounter();
 
-        // Start the timer if it is the first click
+        
         if (moveCounter === 1) {
             timeInt = setInterval(startTimer, 1000);
         }
-        // if two cards are open
+        
         if(openedCards.length === 2){
-            // if the two opened cards match
+           
             if(openedCards[0] === openedCards[1]){
-                // Reset the failed match count back to 0
+                
                 tryCounter = 0;
                 lockMatch();
                 removeOpenedList();
-                // if all 16 cards are matched, stop the timer and display congrats
+                
                 if (match === 16){
                     stopTimer();
-                    // Allow time for the matching animation to finish before display popup
+                    
                     setTimeout(function() {
                         return displayCongrats();}, 900
                     );
                 }
-            } else {  // if the two opened cards do not match
-                // hide the cards after 1 second to allow user time to see the symbols
+            } else {  
                 setTimeout(function(){
                     return hideCards();}, 900
                 );
-                // remove the cards from the list of open cards
+                
                 setTimeout(function() {
                     return removeOpenedList();}, 900);
 
-                // Lower the stars if user has viewed 8 cards, and the 4 recent clicks are failed matches
-                // Do not lower the star rating if the rating is 1
+                
                 if ((moveCounter >= 8) && (tryCounter >= 4) && (starRating > 1)){
                     lowerStars();
                 }
@@ -174,7 +152,7 @@ function processClick() {
     }
 }
 
-// Change the text of the timer on the webpage to reflect elapsed time in minutes and seconds
+
 function startTimer(){
     ++totalSeconds;
     function addZero(i) {
@@ -185,7 +163,7 @@ function startTimer(){
     timer.innerHTML = `${min}:${sec}`;
 }
 
-// Reset the timer to default of 0 and the text on the webpage to 00:00
+// Reset the timer to default of 0
 function resetTimer(){
     clearInterval(timeInt);
     totalSeconds = 0;
@@ -197,12 +175,12 @@ function stopTimer(){
     clearInterval(timeInt);
 }
 
-// Show the card by adding 'open' and 'show' class name
+
 function displayCard(item) {
     item.className = "card open show";
 }
 
-// Hide opened cards by removing the 'open' and 'show' class name
+
 function hideCards() {
     var openClass = document.getElementsByClassName("open");
     while (openClass.length){
@@ -210,19 +188,19 @@ function hideCards() {
     }
 }
 
-// Return true if the item is already opened and false if not
+
 function isSameCard(item) {
     const isSame = (item.className === "card open show") ? true : false;
     return isSame;
 }
 
-// Return true if the item is already matched and false if not
+
 function isAlreadyMatched(item) {
     const isAM = (item.className === "card match") ? true : false;
     return isAM;
 }
 
-// Add the item to a list of opened symbols
+
 function addOpenedList(item) {
     var inner = item.childNodes;
     for (var i=0; i<inner.length; i++){
@@ -233,19 +211,17 @@ function addOpenedList(item) {
     }
 }
 
-// Increase the click(move) count by 1 and update the HTML text to the current value
+
 function incrementCounter() {
     moveCounter++;
     moves[0].innerHTML = moveCounter;
 }
 
-// Reset the click(move) to 0 and update the HTML text to the current value
 function resetCounter() {
     moves[0].innerHTML = moveCounter = 0;
 }
 
-// Keep the matched cards opened by setting the class name to 'card match'
-// Increase the match count by 2
+
 function lockMatch() {
     var fa = `fa-${openedCards[0]}`;
     var collection = document.getElementsByClassName(`${fa}`);
@@ -256,25 +232,13 @@ function lockMatch() {
     matchCounter += 2;
 }
 
-// Remove the two items in the list of opened card symbols
+
 function removeOpenedList() {
     openedCards.pop();
     openedCards.pop();
 }
 
-// Create a div element to add to the page that will hold the congrats message later
-// Hide the div element initially
-//function build congrats
 
-
-// Display the congrats message with the move count, total time, star function displayCongrats() {
-    
-
-// Hide the congrats popup by adding the class 'dimmed'
-// Erase the congrats text messages
-//function hide congrats
-
-// Lower the star rating by one, and hide the last star by adding the class 'dimmed'
 function lowerStars() {
     starRating--;
     tryCounter = 0;
@@ -282,7 +246,7 @@ function lowerStars() {
     stars[starRating].className = "fa fa-star dimmed";
 }
 
-// Reset the rating to 3 and show all stars by removing the class 'dimmed'
+
 function resetStars() {
     starRating = 3;
     var stars = document.getElementsByClassName("fa-star");
